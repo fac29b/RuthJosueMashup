bf92ce736e4645b600a04130a0859590e27db8b3
josueJURE <josue.jure@gmail.com>
2024-01-21 13:11:51 +0000

    (feat CSS): create header for weather API

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express');
+import express from "express";
+import fetch from 'node-fetch';
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
+  res.send("Hello Express")
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  console.log(`server is listening on ${port} `)
+})
+
+const url = 'https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D?units=auto&lang=en'
+const options = {
+  method: "GET",
+  headers: {
+    'X-RapidAPI-Key': '868db2c1eamshd290e20c8dc8261p1627dejsnf52bb1929fb7',
+    'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
+  }
+};
+
+
+
